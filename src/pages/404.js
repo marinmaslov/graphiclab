d615628ec0a73586391eb04373fbdf29e067f116
marinmaslov <marinmaslov.14@gmail.com>
2020-10-15 11:39:03 +0200

    Initial clean commit for GraphicLab App

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const NotFoundPage = () => (
+    <h1>
+      404: Not Found
+    </h1>
+)
+
+export default NotFoundPage
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => (
+  <p>
+    Test
+  </p>
+)
+
+export default IndexPage
