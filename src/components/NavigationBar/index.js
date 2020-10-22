d089fe512ded1a58051702c1d54ebef28b9b2f6f
Marin Maslov <marin.maslov@ericsson.com>
2020-10-22 10:56:32 +0200

    Added NavigationBar Navigation and Combobox

---
+++ src/components/NavigationBar/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import styles from './style.module.css'
+
+const NavigationBar = () => <section></section>
+
+export default NavigationBar
--- src/components/NavigationHeader/index.js
+++ src/components/NavigationHeader/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import Logo from '../Logo'
+import Navigation from '../Navigation'
 
 import styles from './style.module.css'
 
 const NavigationHeader = () => (
   <section className={styles.navigationHeader}>
     <Logo />
+    <Navigation />
   </section>
 )
 export default NavigationHeader
