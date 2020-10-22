39c76df905a8aa90bb9933abd3d1872f5e2a30a3
Marin Maslov <marin.maslov@ericsson.com>
2020-10-22 11:04:08 +0200

    refactor tabs to use map()

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
+ 
 import styles from './style.module.css'
+
+const navTabs = ['Home', 'Accommodation', 'Photo Gallery', 'Contact']
 
 const NavigationBar = () => (
     <nav className={styles.navigationBar}>
-        <li className={styles.active}>Home</li>
-        <li>Accommodation</li>
-        <li>Photo gallery</li>
-        <li>Contact</li>
-    </nav>
+    {navTabs.map(tab => <li className={tab==='Home' ? styles.active : ''}>
+      {tab}</li>
+    )}
+  </nav>
 )
 
 export default NavigationBar
