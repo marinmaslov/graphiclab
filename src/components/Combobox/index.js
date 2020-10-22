438297c548002157d1d6be303894e87c95936b13
Marin Maslov <marin.maslov@ericsson.com>
2020-10-22 11:14:46 +0200

    adding Combobox

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-
+ 
 import styles from './style.module.css' 
 
-const Combobox = () => <section></section>
+const Combobox = () => <span className={styles.combobox}>English</span>
 
 export default Combobox
