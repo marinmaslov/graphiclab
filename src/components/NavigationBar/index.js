aaf46ca6eca39b66f5732b55ef43d9e587daa41e
Marin Maslov <marin.maslov@ericsson.com>
2020-10-22 11:00:32 +0200

    adding NavigationBar part 1

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 import styles from './style.module.css'
 
-const NavigationBar = () => <section></section>
+const NavigationBar = () => (
+    <nav className={styles.navigationBar}>
+        <li className={styles.active}>Home</li>
+        <li>Accommodation</li>
+        <li>Photo gallery</li>
+        <li>Contact</li>
+    </nav>
+)
 
 export default NavigationBar
