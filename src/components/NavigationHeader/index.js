fe3b6a25a9be7b29fcdde597ec34ad8ccc3697dd
Marin Maslov <marin.maslov@ericsson.com>
2020-10-22 10:51:30 +0200

    Added Navigation and Logo

---
+++ src/components/NavigationHeader/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Logo from '../Logo'
+
+import styles from './style.module.css'
+
+const NavigationHeader = () => (
+  <section className={styles.navigationHeader}>
+    <Logo />
+  </section>
+)
+export default NavigationHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 import ContactBar from '../components/contactBar'
+import NavigationHeader from '../components/NavigationHeader'
 
 const IndexPage = () => (
   <main>
     <ContactBar />
+    <NavigationHeader />
   </main>
 )
 
