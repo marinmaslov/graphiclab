372f24929a816fe03e2b21437bc0918be7f006d6
marinmaslov <marinmaslov.14@gmail.com>
2020-12-02 18:42:54 +0100

    + [ADD] Fixing some components, adding new ones and addin CSS

--- graphiclab/src/components/layout/base/base.js
+++ graphiclab/src/components/layout/base/base.js
@@ -7,6 +7,7 @@ import Helmet from "react-helmet"
 
 import "./base.css"
 
+import Container from "./components/container/container"
 import Header from "./components/header/header"
 import Footer from "./components/footer/footer"
 
@@ -18,7 +19,11 @@ const Base = ({ children }) => {
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"></link>
         </Helmet>
         <Header/>
-        <main>{children}</main>
+          <Container>
+            <main>
+              {children}
+            </main>
+          </Container>
         <Footer/>
     </>
   )
