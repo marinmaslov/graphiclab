ad6203421936d2a245e884370929201fe3b073aa
marinmaslov <marinmaslov.14@gmail.com>
2020-12-03 15:35:46 +0100

    + [FIX] Fixing some CSS issues related to navigation bar

--- graphiclab/src/pages/404.js
+++ graphiclab/src/pages/404.js
@@ -1,9 +1,12 @@
 import React from "react"
 
+import Layout from "../components/layout/base/base"
 import PageNotFound from "../components/layout/404/404"
 
 const NotFoundPage = () => (
-  <PageNotFound/>
+  <Layout>
+    <PageNotFound/>
+  </Layout>
 )
 
 export default NotFoundPage
