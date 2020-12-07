f76b054741c32d569a5bbea7e108cfd8bb78b73c
aturic <ante.turic@gmail.com>
2020-12-07 21:28:46 +0100

    + [ADD] Addin Contest component

---
+++ graphiclab/src/components/contest/contest.js
@@ -0,0 +1,93 @@
+/**
+ * Home page product contest
+ */
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+import Card from "./card/card"
+
+import "./contest.css"
+
+const query = graphql`{
+    stickers: allContentfulProductContest(limit: 3, sort: {fields: [year, month], order: DESC}) {
+        edges {
+            node {
+                id
+                name
+                product {
+                    id
+                    image {
+                        fluid {
+                          src
+                        }
+                    }
+                }
+            }
+        }
+    },
+    shirts: allContentfulShirtContest(limit: 3, sort: {fields: [year, month], order: DESC}) {
+        edges {
+            node {
+                id
+                name
+                product {
+                    id
+                    image {
+                        fluid {
+                          src
+                        }
+                    }
+                }
+            }
+        }
+    }
+}`
+
+const Contest = ({ contest }) => {
+    return (
+        <StaticQuery on
+        query = { query }
+        render = { data => {
+            return (
+                <section id="contest">
+                    <div className="heading">
+                            { contest === "sticker" &&
+                                <> 
+                                    <h1>Sticker contest</h1>
+                                    <p>The three best stickers of the month based on your choice.</p>
+                                </>
+                            } 
+                            { contest === "shirt" &&
+                                <> 
+                                    <h1>Shirt contest</h1>
+                                    <p>The three best T-shirts of the month based on your choice.</p>
+                                </>
+                            } 
+                    </div>
+                    <div className="container">
+                        <div className="cards">
+                            { contest === "sticker" &&
+                                <> 
+                                    {data.stickers.edges.map(({ node: product }) => {
+                                        return <Card key={ product.id } product={ product }/>
+                                    })}
+                                </>
+                            } 
+                            { contest === "shirt" &&
+                                <> 
+                                    {data.shirts.edges.map(({ node: product }) => {
+                                        return <Card key={ product.id } product={ product }/>
+                                    })}
+                                </>
+                            } 
+                        </div>
+                    </div>
+                </section>
+            )
+        }}
+        />
+
+    )
+}
+
+export default Contest
--- graphiclab/src/pages/index.js
+++ graphiclab/src/pages/index.js
@@ -2,15 +2,15 @@ import React from "react"
 
 import Layout from "../components/layout/base/base"
 import Banner from "../components/banners/particles/banner"
-
+import Contest from "../components/contest/contest"
 import Newsletter from "../components/newsletter/newsletter"
 
 
 const IndexPage = () => (
   <Layout isContainer={false}>
     <Banner/>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <Contest contest="sticker"/>
+    <Contest contest="shirt"/>
     <Newsletter/>
   </Layout>
 )
