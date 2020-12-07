eeb4cdb7b2b85a1be41deb9de4311ad326217223
aturic <ante.turic@gmail.com>
2020-12-07 21:22:23 +0100

    + [ADD] Adding Newsletter component

--- graphiclab/src/pages/index.js
+++ graphiclab/src/pages/index.js
@@ -3,12 +3,15 @@ import React from "react"
 import Layout from "../components/layout/base/base"
 import Banner from "../components/banners/particles/banner"
 
+import Newsletter from "../components/newsletter/newsletter"
+
 
 const IndexPage = () => (
   <Layout isContainer={false}>
     <Banner/>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <Newsletter/>
   </Layout>
 )
 
