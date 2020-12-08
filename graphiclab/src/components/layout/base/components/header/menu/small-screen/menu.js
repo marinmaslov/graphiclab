747cdbdbca63b6ed95d0c5610c68fc677f1252ee
aturic <ante.turic@gmail.com>
2020-12-08 13:17:29 +0100

    + [FIX] Fixed issue with navigation hovering underline CSS

--- graphiclab/src/components/layout/base/components/header/menu/small-screen/menu.js
+++ graphiclab/src/components/layout/base/components/header/menu/small-screen/menu.js
@@ -34,22 +34,22 @@ export default class SmallMenu extends React.Component {
                         <p className="mobile-menu-icon" onClick={this.handleMenuButtonClick}><i class="fas fa-times"></i></p>
                         <br/>
                         <Link className="menu-item" to="/" onClick={() => this.closeMenu() }>
-                            <p className="menu-item-element menu-item-plain"><i class="fas fa-home"></i><p>Home</p></p>
+                            <p className="menu-item-element menu-item-plain"><i class="fas fa-home" style={{ textDecoration: "none!important" }}></i><span>Home</span></p>
                         </Link>
                         <Link className="menu-item" to="/blog" onClick={() => this.closeMenu() }>
-                            <p className="menu-item-element menu-item-plain"><i class="fas fa-book"></i><p>Blog</p></p>
+                            <p className="menu-item-element menu-item-plain"><i class="fas fa-book"></i><span>Blog</span></p>
                         </Link>
                         <Link className="menu-item" to="/contacts" onClick={() => this.closeMenu() }>
-                            <p className="menu-item-element menu-item-plain"><i class="fas fa-address-book"></i><p>Contacts</p></p>
+                            <p className="menu-item-element menu-item-plain"><i class="fas fa-address-book"></i><span>Contacts</span></p>
                         </Link>
                         <Link className="menu-item" to="/about" onClick={() => this.closeMenu() }>
-                            <p className="menu-item-element menu-item-plain"><i class="fas fa-info-circle"></i><p>About</p></p>
+                            <p className="menu-item-element menu-item-plain"><i class="fas fa-info-circle"></i><span>About</span></p>
                         </Link>
                         <Link className="menu-item" to="/store" onClick={() => this.closeMenu() }>
-                            <p className="menu-item-element shop"><i class="fas fa-shopping-cart"></i> <p>Store</p></p>
+                            <p className="menu-item-element shop"><i class="fas fa-shopping-cart"></i><span>Store</span></p>
                         </Link>
                         <Link className="menu-item" to="/login" onClick={() => this.closeMenu() }>
-                            <p className="menu-item-element login"><i class="fas fa-user"></i> <p>Login</p></p>
+                            <p className="menu-item-element login"><i class="fas fa-user"></i><span>Login</span></p>
                         </Link>
                     </div>
                 </nav>
