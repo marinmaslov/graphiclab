7c2db050b87bf79416582039413b8034d5e5f55f
marinmaslov <marinmaslov13@gmail.com>
2020-12-09 22:19:52 +0100

    + [FIX] Fixing issues with wed.dev meassures

--- graphiclab/src/components/banners/particles/components/carousel.js
+++ graphiclab/src/components/banners/particles/components/carousel.js
@@ -2,18 +2,35 @@
  * Carousel component that is part of Banner 
  */
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
 
 import "./carousel.css"
-import Image from "../../../../images/stickers/sticker_003.png"
 
 const Carousel = ({ }) => {
     return (
-        <>
         <div class="carousel">
-            <img src={Image} alt={ "sticker" }/>
+            <StaticQuery on
+                query = { query }
+                render = { data => {
+                    return (
+                        <>
+                            <img src={ data.contentfulStickerImages.image.fluid.src } alt={ "sticker" }/>
+                        </>
+                    )}  
+                } 
+            /> 
         </div>
-        </>
     )
 }
 
 export default Carousel
+
+export const query = graphql`{
+    contentfulStickerImages(name: {eq: "banner"}) {
+        image {
+            fluid(quality: 30, maxHeight: 300, maxWidth: 300) {
+                src
+            }
+        }
+    }
+}`
--- graphiclab/src/components/layout/base/components/header/logo/logo.js
+++ graphiclab/src/components/layout/base/components/header/logo/logo.js
@@ -2,18 +2,34 @@
  * Logo component of Header
  */
 import React from "react"
-
-import Logo from "../../../../../../images/logo/logo.png"
+import { graphql, StaticQuery } from "gatsby"
 
 const MainLogo = ({ }) => {
     return (
-        <>
-            <a href="/" class="logo">
-                <img src={ Logo } alt={ "logo" } />
-                <span><strong>Graphic</strong>Lab</span>
-            </a>
-        </>
+        <a href="/" class="logo">
+            <StaticQuery on
+                query = { query }
+                render = { data => {
+                    return (
+                        <>
+                            <img src={ data.contentfulStickerImages.image.fluid.src } alt={ "logo" } />
+                        </>
+                    )}  
+                } 
+            /> 
+            <span><strong>Graphic</strong>Lab</span>
+        </a>
     )
 }
 
 export default MainLogo
+
+export const query = graphql`{
+    contentfulStickerImages(name: {eq: "logo"}) {
+        image {
+            fluid(quality: 30, maxHeight: 50, maxWidth: 50) {
+                src
+            }
+        }
+    }
+}`
