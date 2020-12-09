6b8796c9b03494ba3c333440185c30561a1ee7e0
marinmaslov <marinmaslov13@gmail.com>
2020-12-10 00:40:00 +0100

    + [REMOVE] Removing lazy rendering as it is not ssr supported

--- graphiclab/src/components/banners/particles/banner.js
+++ graphiclab/src/components/banners/particles/banner.js
@@ -1,26 +1,18 @@
 /**
  * Banner component 
  */
-import React, { Suspense } from "react"
-import lazy from 'react-lazy-ssr';
+import React from "react"
 
 import Text from "../particles/components/text"
 import Carousel from "../particles/components/carousel"
 
 import "./banner.css"
 
-const Particles = lazy(
-  () => import ("./components/particles/particles"),
-  { chunkName: 'Particles' }
-)
-
 const Banner = ({ }) => {
     return (
         <>
         <section id="banner">
-            <Suspense fallback={""}>
-              <Particles />
-            </Suspense>
+            <Particles />
             <div class="container">
                 <Text/>
                 <Carousel/>
