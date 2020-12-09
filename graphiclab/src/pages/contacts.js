80a5d2216db7ee5d14766186510777a89565914c
marinmaslov <marinmaslov13@gmail.com>
2020-12-10 00:01:16 +0100

    + [ADD] Adding Google Maps component

--- graphiclab/src/pages/contacts.js
+++ graphiclab/src/pages/contacts.js
@@ -3,28 +3,13 @@ import React from "react"
 import Layout from "../components/layout/base/base"
 import Banner from "../components/banners/custom/banner"
 import Container from "../components/layout/base/components/container/container"
-import PropTypes from 'prop-types'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faFax, faMapMarkedAlt, faPhone } from '@fortawesome/free-solid-svg-icons'
 
+import Map from "../components/map/map"
+
 export default class ContactsPage extends React.Component {
-  static propTypes = {
-    /** Latitude and Longitude of the map centre in an array, eg [51, -1] **/
-    position: PropTypes.array,
-    /** Initial zoom level for the map (default 13) **/
-    zoom: PropTypes.number,
-    /** If set, will display a marker, which when clicked will display this text **/
-    markerText: PropTypes.string
-  }
-
-  static defaultProps = {
-    position: [43.511445, 16.469166],
-    zoom: 15,
-    markerText: "GraphicLab HC, Ul. Ruđera Boškovića 32, 21000, Split"
-  }
-
   render(){
     return(
       <Layout isContainer={false}>
@@ -39,22 +24,7 @@ export default class ContactsPage extends React.Component {
                   <p><i><FontAwesomeIcon icon={ faMapMarkedAlt }/></i>R. Boskovića 32, 21 000 Split, Croatia</p>
                 </div>
                 <div class="lefelet-container">
-                  <MapContainer
-                    className="lefelet-container"
-                    center={[43.511445, 16.469166]}
-                    zoom={15}
-                    maxZoom={35}
-                  >
-                    <TileLayer
-                      url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    />
-                    {this.props.markerText !== "" &&
-                      <Marker position={this.props.position}>
-                        <Popup>{this.props.markerText}</Popup>
-                      </Marker>
-                    }
-                  </MapContainer>
+                  <Map/>
                 </div>
           </Container>
       </Layout>
